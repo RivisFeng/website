0eeb13bd1b9e5102353423f51fec424feac95957
RivisFeng <RivisFeng@outlook.com>
2018-04-25 15:43:24 +0800

    office

---
+++ website/src/main/java/com/rivis/feng/website/Service/UserService.java
@@ -0,0 +1,16 @@
+package com.rivis.feng.website.Service;
+
+import com.rivis.feng.website.pojo.dto.LoginInDto;
+import com.rivis.feng.website.pojo.dto.ResultDataDto;
+
+public interface UserService {
+
+    /**
+     * web用户登录
+     *
+     * @author Rivis
+     * @param loginInDto {LoginInDto}
+     * @return {ResultDataDto}
+     */
+    ResultDataDto loginIn(LoginInDto loginInDto);
+}
