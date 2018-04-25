90340d5135de87fc36fb41866058005ad0951f3c
RivisFeng <RivisFeng@outlook.com>
2018-04-25 15:56:23 +0800

    office

---
+++ website/src/main/java/com/rivis/feng/website/pojo/dto/LoginInDto.java
@@ -0,0 +1,52 @@
+package com.rivis.feng.website.pojo.dto;
+
+public class LoginInDto {
+
+    private String userPhone;
+
+    private String userPassword;
+
+    private String checkCode;
+
+    public LoginInDto() {
+    }
+
+    public LoginInDto(String userPhone, String userPassword, String checkCode) {
+        this.userPhone = userPhone;
+        this.userPassword = userPassword;
+        this.checkCode = checkCode;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getCheckCode() {
+        return checkCode;
+    }
+
+    public void setCheckCode(String checkCode) {
+        this.checkCode = checkCode;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginInDto{" +
+                "userPhone='" + userPhone + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", checkCode='" + checkCode + '\'' +
+                '}';
+    }
+}
