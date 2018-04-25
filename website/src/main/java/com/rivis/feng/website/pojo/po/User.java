b9aaffdc8e82324b4447b4e4d7a0e0a35b542271
RivisFeng <RivisFeng@outlook.com>
2018-04-25 13:37:32 +0800

    反向工程生成的基本文件

--- website/src/main/java/com/rivis/feng/website/pojo/po/User.java
+++ website/src/main/java/com/rivis/feng/website/pojo/po/User.java
@@ -1,18 +1,42 @@
 package com.rivis.feng.website.pojo.po;
 
+import java.util.Date;
 
 public class User {
+    private String userId;
 
     private String userName;
 
     private String userPassword;
+
+    private Date userBirthday;
+
+    private String userGender;
+
+    private String userPhone;
+
+    private String userAddress;
+
+    private Integer userDelete;
+
+    private Integer userVip;
+
+    private String userMail;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
 
     public String getUserName() {
         return userName;
     }
 
     public void setUserName(String userName) {
-        this.userName = userName;
+        this.userName = userName == null ? null : userName.trim();
     }
 
     public String getUserPassword() {
@@ -20,6 +44,62 @@ public class User {
     }
 
     public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
+        this.userPassword = userPassword == null ? null : userPassword.trim();
+    }
+
+    public Date getUserBirthday() {
+        return userBirthday;
+    }
+
+    public void setUserBirthday(Date userBirthday) {
+        this.userBirthday = userBirthday;
+    }
+
+    public String getUserGender() {
+        return userGender;
+    }
+
+    public void setUserGender(String userGender) {
+        this.userGender = userGender == null ? null : userGender.trim();
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone == null ? null : userPhone.trim();
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress == null ? null : userAddress.trim();
+    }
+
+    public Integer getUserDelete() {
+        return userDelete;
+    }
+
+    public void setUserDelete(Integer userDelete) {
+        this.userDelete = userDelete;
+    }
+
+    public Integer getUserVip() {
+        return userVip;
+    }
+
+    public void setUserVip(Integer userVip) {
+        this.userVip = userVip;
+    }
+
+    public String getUserMail() {
+        return userMail;
+    }
+
+    public void setUserMail(String userMail) {
+        this.userMail = userMail == null ? null : userMail.trim();
     }
 }
